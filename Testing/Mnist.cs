04d17e093269aab99ec7950104c1db628792f3ba
AndrewLevada <zipeofficial@gmail.com>
2020-08-07 10:51:47 +0300

    Excel logging added

--- Testing/Mnist.cs
+++ Testing/Mnist.cs
@@ -4,21 +4,34 @@ using System.IO;
 using System.Linq;
 using NeuralNetworks;
 using NeuralNetworks.Misc;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace Testing {
 	internal static class Mnist {
+		private static string experimentTitle;
 		private static List<NeuralNetwork> nns;
 		
 		public static void Run() {
-			Console.Write("Initialisation of MNIST 784+300+10 test... ");
+			Console.Write("Hello. Enter experiment title: ");
+			experimentTitle = Console.ReadLine();
+			
+			Console.Write("Initialisation of NNs...");
 			SetupNNs();
-			Console.WriteLine("Done");
+			Console.WriteLine(" Done.");
 			
 			Console.WriteLine("Training started!");
+			Console.Write("In progress");
 			Train();
+			Console.WriteLine(" Done.");
 
 			Console.WriteLine("Testing started!");
+			Console.Write("In progress");
 			Test();
+			Console.WriteLine(" Done.");
+			
+			Console.WriteLine("Process is completed and result is saved in excel file.");
+			Console.ReadKey();
 		}
 
 		private static void SetupNNs() {
@@ -44,12 +57,12 @@ namespace Testing {
 			trainLabels.Read(new byte[4 * 2], 0, 4 * 2);
 			
 			EList<double> answer = new EList<double>();
-			for (int i = 0; i < 10; i++) answer.Add(0);
+			for (int i = 0; i < 10; i++) answer.Add(0.5);
 			
 			EList<double> input = new EList<double>();
 			for (int i = 0; i < 784; i++) input.Add(0);
 			
-			for (int h = 0; h < 10; h++) {
+			for (int h = 0; h < 15; h++) {
 				for (int i = 0; i < 1000; i++) {
 					int digit = GetNextByte(trainLabels);
 				
@@ -63,11 +76,11 @@ namespace Testing {
 					}
 
 					answer[digit] = 1;
-					foreach (NeuralNetwork nn in nns) nn.Backpropagate(answer, 10);
-					answer[digit] = 0;
+					foreach (NeuralNetwork nn in nns) nn.Backpropagate(answer, 1);
+					answer[digit] = 0.5;
 				}
 				
-				Console.WriteLine($"Train iteration {h * 1000}");
+				Console.Write(".");
 			}
 		}
 
@@ -81,35 +94,71 @@ namespace Testing {
 			for (int i = 0; i < 784; i++) input.Add(0);
 			
 			List<int> numOfGood = nns.Select(nn => 0).ToList();
+			List<List<KeyValuePair<int, int>>> memory = 
+				nns.Select(nn => new List<KeyValuePair<int, int>>()).ToList();
 
-			for (int i = 0; i < 5000; i++) {
-				int digit = GetNextByte(testLabels);
-				
-				byte[] byteInput = new byte[784];
-				testImages.Read(byteInput, 0, 784);
-				for (int j = 0; j < 784; j++) input[j] = byteInput[j] / 255d;
+			for (int h = 0; h < 5; h++) {
+				for (int i = 0; i < 1000; i++) {
+					int digit = GetNextByte(testLabels);
 
-				for (int index = 0; index < nns.Count; index++) {
-					NeuralNetwork nn = nns[index];
-					
-					nn.PutData(input);
-					nn.Run();
+					byte[] byteInput = new byte[784];
+					testImages.Read(byteInput, 0, 784);
+					for (int j = 0; j < 784; j++) input[j] = byteInput[j] / 255d;
 
-					int maxIndex = nn.GetMaxIndexInOutput();
-					if (maxIndex == digit) numOfGood[index]++;
+					for (int nnIndex = 0; nnIndex < nns.Count; nnIndex++) {
+						NeuralNetwork nn = nns[nnIndex];
+
+						nn.PutData(input);
+						nn.Run();
+
+						int answer = nn.GetMaxIndexInOutput();
+						if (answer == digit) numOfGood[nnIndex]++;
+						
+						memory[nnIndex].Add(new KeyValuePair<int, int>(answer, digit));
+					}
 				}
-
-				// Console.WriteLine($"Iteration {i} has {digit[0]} {maxIndex}");
+				Console.Write(".");
 			}
-
-			for (int index = 0; index < nns.Count; index++) 
-				Console.WriteLine($"Results for {index} are: {numOfGood[index]} out of 5000 => {numOfGood[index] / 5000d}");
+			Console.WriteLine(" Done.");
+			
+			Console.Write("Writing data to excel... ");
+			WriteToExcel(numOfGood, memory);
 		}
 
 		private static int GetNextByte(Stream fileStream) {
 			byte[] digit = new byte[1];
 			fileStream.Read(digit, 0, 1);
 			return digit[0];
+		}
+
+		private static void WriteToExcel(List<int> numOfGood, List<List<KeyValuePair<int, int>>> memory) {
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+			using ExcelPackage package = new ExcelPackage(new FileInfo("results.xlsx"));
+			ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(experimentTitle);
+
+			for (int i = 0; i < nns.Count; i++) {
+				int currentColumn = i * 2 + 1;
+				
+				worksheet.Cells[1, currentColumn].Value = "ANS" + (i + 1);
+				worksheet.Cells[1, currentColumn].Style.Font.Bold = true;
+				worksheet.Cells[1, currentColumn + 1].Value = "EXP" + (i + 1);
+				worksheet.Cells[1, currentColumn + 1].Style.Font.Bold = true;
+
+				worksheet.Cells[2, currentColumn].Value = numOfGood[i];
+				worksheet.Cells[2, currentColumn + 1].Value = memory[i].Count;
+				worksheet.Cells[3, currentColumn + 1].Value = numOfGood[i] * 1d / memory[i].Count;
+
+				for (int index = 0; index < memory[i].Count; index++) {
+					KeyValuePair<int, int> pair = memory[i][index];
+
+					worksheet.Cells[4 + index, currentColumn].Value = pair.Key;
+					worksheet.Cells[4 + index, currentColumn + 1].Value = pair.Value;
+				}
+
+				worksheet.Column(currentColumn + 1).Style.Border.Right.Style = ExcelBorderStyle.Thin;
+			}
+			
+			package.Save();
 		}
 	}
 }
