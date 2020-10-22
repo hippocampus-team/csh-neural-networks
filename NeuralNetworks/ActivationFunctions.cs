86ef525522a87d98f752c6ab2f0cc3ec87361134
AndrewLevada <zipeofficial@gmail.com>
2020-10-22 20:51:41 +0300

    Tanh activation function added

--- NeuralNetworks/ActivationFunctions.cs
+++ NeuralNetworks/ActivationFunctions.cs
@@ -12,7 +12,10 @@ public class Sigmoid : ActivationFunction {
 		if (value < -50.0) return 0.0;
 		if (value > 50.0) return 1.0;
 		
-		return 1.0d / (1.0d + Math.Exp(-value));
+		// Equivalent to 1.0d / (1.0d + Math.Exp(-value));
+		
+		double k = Math.Exp(value);
+		return k / (1.0d + k);
 	}
 
 	public double countDerivative(double value) {
@@ -29,8 +32,24 @@ public class ModifiedSigmoid : ActivationFunction {
 		double k = Math.Exp(value);
 		return k / (1.0d + k);
 	}
-
+	
 	public double countDerivative(double value) => value * (1 - value);
+}
+
+public class HyperbolicTangent : ActivationFunction {
+	public double count(double value) {
+		if (value < -50.0) return 0.0;
+		if (value > 50.0) return 1.0;
+
+		double a = Math.Exp(value);
+		double b = Math.Exp(-value);
+		return (a - b)/(a + b);
+	}
+
+	public double countDerivative(double value) {
+		double tanh = count(value);
+		return 1 - tanh * tanh;
+	}
 }
 
 }
