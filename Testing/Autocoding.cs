3b871d4040c5c004395f4b685523ea84f8511472
AndrewLevada <zipeofficial@gmail.com>
2020-11-06 12:14:42 +0300

    Const naming changed

--- Testing/Autocoding.cs
+++ Testing/Autocoding.cs
@@ -5,24 +5,24 @@ using NeuralNetworks.Misc;
 namespace Testing {
 
 internal static class Autocoding {
-	private const int TEST_NUM = 128;
+	private const int testNum = 128;
 
 	public static void run() {
 		NeuralNetwork nn = new NeuralNetwork();
 
-		nn.setInputLength(TEST_NUM);
+		nn.setInputLength(testNum);
 		nn.addDenseLayer(16, new Sigmoid());
-		nn.addDenseLayer(TEST_NUM, new Sigmoid());
+		nn.addDenseLayer(testNum, new Sigmoid());
 
 		nn.fillRandomWeights();
 		nn.fillRandomBiases();
 
 		Random random = new Random(Guid.NewGuid().GetHashCode());
 		EList<double> data = new EList<double>();
-		for (int i = 0; i < TEST_NUM; i++) data.Add(0);
+		for (int i = 0; i < testNum; i++) data.Add(0);
 
 		for (int i = 0; i < 1000; i++) {
-			for (int j = 0; j < TEST_NUM; j++) data[j] = Math.Round(random.NextDouble());
+			for (int j = 0; j < testNum; j++) data[j] = Math.Round(random.NextDouble());
 
 			nn.putData(data);
 			nn.run();
