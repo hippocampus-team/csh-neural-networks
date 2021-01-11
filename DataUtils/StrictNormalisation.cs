50c84c21277601c21c81c52f5a4d2879ed88d609
AndrewLevada <zipeofficial@gmail.com>
2021-01-11 17:54:41 +0300

    Strict dataset normalisation algorithm added

---
+++ DataUtils/StrictNormalisation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DataUtils {
+    public static class StrictDatasetNormalizer {
+        public static void fromStreamReaderToFile(StreamReader data, FileInfo outputFile) {
+            StreamWriter output = new (outputFile.FullName);
+            Dictionary<int, Stack<string>> sortedData = new ();
+            
+            // Copy header line
+            output.WriteLine(data.ReadLine());
+
+            while (!data.EndOfStream) {
+                string entery = data.ReadLine();
+                if (entery == null) break;
+                int label = int.Parse(entery.Substring(entery.LastIndexOf(',') + 1));
+
+                if (sortedData.ContainsKey(label)) sortedData[label].Push(entery);
+                else sortedData.Add(label, new Stack<string>(new [] { entery }));
+            }
+
+            int minLabelAmount = sortedData.Min(pair => pair.Value.Count);
+
+            for (int i = 0; i < minLabelAmount; i++)
+                foreach (Stack<string> labelEntries in sortedData.Values)
+                    output.WriteLine(labelEntries.Pop());
+
+            output.Close();
+        }
+    }
+}
---
+++ Testing/DataToolsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using DataUtils;
+
+namespace Testing {
+
+public class DataToolsTest {
+	public static void run() {
+		FileInfo inputFile = new ("data/heart.csv");
+		FileInfo outputFile = new ("data/normal.csv");
+		StrictDatasetNormalizer.fromStreamReaderToFile(inputFile.OpenText(), outputFile);
+		Console.WriteLine("DONE");
+	}
+}
+
+}
