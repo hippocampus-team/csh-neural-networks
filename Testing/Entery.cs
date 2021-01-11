32b0d3a6a3b02006f2dba9759cd269e8db99080b
AndrewLevada <zipeofficial@gmail.com>
2021-01-11 18:05:45 +0300

    Dotnet switched to 5

--- Testing/Entery.cs
+++ Testing/Entery.cs
@@ -2,7 +2,8 @@
 
 internal static class Entery {
 	// ReSharper disable once InconsistentNaming
-	private static void Main(string[] args) => Deserializer.run();
+	private static void Main(string[] args) => DataToolsTest.run();
+
 }
 
 }
