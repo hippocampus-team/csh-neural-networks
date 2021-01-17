2c4facc0fd8c76a9bd1e501d9571d6032dd1c740
AndrewLevada <zipeofficial@gmail.com>
2021-01-17 21:59:22 +0300

    Connection between lib and hub established

--- ServerHost/Requests.cs
+++ ServerHost/Requests.cs
@@ -4,6 +4,7 @@ using System.Text;
 namespace ServerHost {
 public static class Requests {
 	public static void api(HttpListenerRequest req, HttpListenerResponse res) {
+		res.Headers.Add("Access-Control-Allow-Origin", "*");
 		res.StatusCode = (int) HttpStatusCode.OK;
 		res.ContentType = "application/json";
 		res.ContentEncoding = Encoding.UTF8;
@@ -13,11 +14,18 @@ public static class Requests {
 	}
 	
 	public static void notFound(HttpListenerRequest req, HttpListenerResponse res) {
+		res.Headers.Add("Access-Control-Allow-Origin", "*");
 		res.StatusCode = (int) HttpStatusCode.NotFound;
 	}
 	
 	public static void getExperimentsList(HttpListenerRequest req, HttpListenerResponse res) {
+		res.Headers.Add("Access-Control-Allow-Origin", "*");
+		res.StatusCode = (int) HttpStatusCode.OK;
+		res.ContentType = "application/json";
+		res.ContentEncoding = Encoding.UTF8;
 		
+		byte[] payload = Encoding.UTF8.GetBytes("[{\"id\":0,\"name\":\"First test\",\"description\":\"Some text\"},{\"id\":1,\"name\":\"One more\",\"description\":\"Very important topic\"}]");
+		res.OutputStream.Write(payload, 0, payload.Length);
 	}
 }
 }
